29b5496a7bf2448c28c23e25ae65c8c42f24bc36
LC <794493138@qq.com>
2024-04-11 22:42:24 +0800

    1.0

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vg@q7iy76q!43r2lt3%(c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
